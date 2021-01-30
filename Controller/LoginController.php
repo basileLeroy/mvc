8b631b60bcc4b5162b224b9c9c21bc6be0d0fed4
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-01-30 13:44:47 +0100

    Replacing login() code with new, more complete code

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -22,20 +22,45 @@ class LoginController
     public function login($role,$email,$password)
     {
 
-        $sql = "SELECT * FROM $role WHERE email=?";
+        $sql = "SELECT * FROM $role WHERE email='$email'";
       
-        $results = $this->databaseManager->database->prepare($sql);
-                    
-        $results->execute([$email]);
+        $databaseUser = $this->databaseManager->database->prepare($sql);
+        $databaseUser->execute();
 
-        foreach($results as $result){
-            if($result["password"]==$password){
-                echo "correct";
-                header("Location: ./View/student_profile.php");
+        $result = $databaseUser->fetch(PDO::FETCH_ASSOC);
+        echo '<pre>';
+        var_dump($databaseUser->fetch(PDO::FETCH_ASSOC));
+        echo '</pre>';
 
-            }else{
-                echo "not correct";
-            };
+        // foreach($databaseUser as $result){
+        //     if($result["password"]==$password){
+        //         echo "correct";
+        //         header("Location: ./View/student_profile.php");
+
+        //     }else{
+        //         echo "not correct";
+        //     };
+        // }
+
+        switch ($role) {
+            case "coaches":
+                if ($result['password'] == $password && $result['email'] == $email) {
+
+                    header("Location: ./View/coach_profile.php");
+                } else {
+
+                    echo "invalid login";
+                }
+                break;
+            case "students":
+                if ($result['password'] == $password && $result['email'] == $email) {
+
+                    header("Location: ./View/student_profile.php");
+                } else {
+
+                    echo "invalid login";
+                }
+                break;
         }
 
          //TODO: need to put error msg if password incorrect + role not match
