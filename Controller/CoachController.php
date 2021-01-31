42b1bbdcea876967fca5e9f72c87590ef10ceea0
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-01-31 16:20:49 +0100

    Cleanup controllers -- creating register.php

--- Controller/CoachController.php
+++ Controller/CoachController.php
@@ -4,7 +4,11 @@ require_once 'Controller/CoachController.php';
 class CoachController
 {
     //TODO: Log in as coach "coachProfile()"
+
+
     //TODO: show all classes in tab "coachOverview()" - display: classes->student names
+    // COACHES SEE ALL CLASSES
+
+
     //TODO: create new challenge "createChallenge()"
-    //TODO: 
 }
