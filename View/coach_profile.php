fbf050db0f5a9eac72bb35c2ebbcc8c4fad3c6e6
chevtong <chevtongwingyan@gmail.com>
2021-02-01 12:41:37 +0100

    Restructure for includes and footer for students

--- View/coach_profile.php
+++ View/coach_profile.php
@@ -3,50 +3,50 @@ require 'includes/header.php';
 require 'includes/nav_coach.php';
 ?>
 
-        <div class="container">
-            <div class="grid">
-            <div class="welcome-msg">
-                <h3>Welcome,
-                    <!-- -->
-                    <?/*= $_GET["user"] */?>
-                </h3>
+<div class="container">
+    <div class="grid">
+        <div class="welcome-msg">
+            <h3>Welcome,
+                <!-- -->
+                <?/*= $_GET["user"] */?>
+            </h3>
 
-                <?php //TODO: to display welcome msg?>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, dolorem!
+            <?php //TODO: to display welcome msg?>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, dolorem!
 
-            </div>
-            <div class="exercise-list">
-                <?php //TODO: for the shortlist of exercises?>
-                <h3>Exercises</h3>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, sint!</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, sint!</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, sint!</p>
-                <a href="">More info<i class="fas fa-plus"></i></a>
-                <button type="submit"><a href="create_challenge.php">Create New Challenge</a></button>
-
-            </div>
-
-            <div class="watch">
-                <?php //TODO: replace the dummy text for the calendar?>
-                <h3>Watch Schedule</h3>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam iusto quam sapiente velit quo non
-                pariatur, eligendi assumenda totam itaque.
-            </div>
-
-            <div class="repo">
-                <?php //TODO: replace the dummy text for the repository link?>
-                <h3>Repository</h3>
-                <a href="">More info<i class="fas fa-plus"></i></a>
-            </div>
-
-            <div class="student-list">
-                <?php //TODO: to display the student list of the same class ?>
-                <h3>Students</h3>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, sint!</p>
-                <a href="">More info<i class="fas fa-plus"></i></a>
-            </div>
-            </div>
         </div>
+        <div class="exercise-list">
+            <?php //TODO: for the shortlist of exercises?>
+            <h3>Exercises</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, sint!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, sint!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, sint!</p>
+            <a href="">More info<i class="fas fa-plus"></i></a>
+            <button type="submit"><a href="create_challenge.php">Create New Challenge</a></button>
+
+        </div>
+
+        <div class="watch">
+            <?php //TODO: replace the dummy text for the calendar?>
+            <h3>Watch Schedule</h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam iusto quam sapiente velit quo non
+            pariatur, eligendi assumenda totam itaque.
+        </div>
+
+        <div class="repo">
+            <?php //TODO: replace the dummy text for the repository link?>
+            <h3>Repository</h3>
+            <a href="">More info<i class="fas fa-plus"></i></a>
+        </div>
+
+        <div class="student-list">
+            <?php //TODO: to display the student list of the same class ?>
+            <h3>Students</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, sint!</p>
+            <a href="">More info<i class="fas fa-plus"></i></a>
+        </div>
+    </div>
+</div>
 
 <?php 
 require 'includes/footer.php';
