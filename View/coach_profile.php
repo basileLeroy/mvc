6b42896aaea4bc267585d3d7264ccb137d7afa9d
chevtong <chevtongwingyan@gmail.com>
2021-02-02 00:04:02 +0100

    Add watch calendar on coach_profile

--- View/coach_profile.php
+++ View/coach_profile.php
@@ -1,10 +1,13 @@
 <?php 
-require 'includes/header.php';
+require 'includes/header_watch.php';
+
 require 'includes/nav_coach.php';
 ?>
+<div class="resize-container">
 
-<div class="container">
-    <div class="grid">
+</div>
+<div class="container-profile">
+    <div class="grid-profile">
         <div class="welcome-msg">
             <h3>Welcome,
                 <!-- -->
@@ -28,9 +31,12 @@ require 'includes/nav_coach.php';
 
         <div class="watch">
             <?php //TODO: replace the dummy text for the calendar?>
+            <div class="container">
             <h3>Watch Schedule</h3>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam iusto quam sapiente velit quo non
-            pariatur, eligendi assumenda totam itaque.
+            <div id="calendar"></div>
+
+            </div>
+            
         </div>
 
         <div class="repo">
@@ -48,6 +54,26 @@ require 'includes/nav_coach.php';
     </div>
 </div>
 
+<script>
+window.addEventListener('DOMContentLoaded', () => {
+
+    var calendar = $('#calendar').fullCalendar({
+
+               //fixedWeekCount: false,
+        editable: false,
+        // height: 400 ,
+        contentHeight: 350,
+        selectable:true,
+        //selectHelper:true,
+       
+        //cannot use PHP tag inside javascript codes, can only use a file return the values
+        events: '../Controller/WatchController.php',
+        displayEventTime: false,
+        eventColor: '#d889a7',
+        eventTextColor: 'white',
+    });
+});
+</script>
 <?php 
 require 'includes/footer.php';
 ?>
